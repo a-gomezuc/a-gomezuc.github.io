dd9b93b683eb6c18072263f3e588a67b6e6cdd6d
Alex Gómez Uceda <alexyuncos96@gmail.com>
2024-01-04 13:13:58 +0100

    Update website

--- js/changeLanguage.js
+++ js/changeLanguage.js
@@ -5,9 +5,9 @@ var en_US = {
   know: "If you want to know more about me, keep scrolling down.",
   about: "About me",
   par1:
-    "My name is Alex Gómez Uceda. I live in a town in Toledo (España) and I am Software Engineering from Rey Juan Carlos University since 2018.",
+    "My name is Alex Gómez Uceda. I live in a town in Toledo (Spain) and I am Software Engineering from Rey Juan Carlos University since 2018.",
   par2:
-    "I consider myself a hard-work, quiet and curious person. Technology and sports are my two main passions. I think that life is a continuous learning and we should never stop learning.",
+    "I consider myself a hard-working, quiet and a very curious person. Technology and sports are my two main passions. I think that life is a continuous learning and we should never stop learning.",
   par3:
     "If you still want to know more about me, you can do it in the last section.",
   contact: "Contact",
@@ -18,20 +18,20 @@ var en_US = {
 
 var es_ES = {
     welcome: "¡Bienvenido!",
-    name: "Mi nombre es Alex Gómez Uceda.",
+    name: "Mi nombre es Alex Gómez Uceda",
     portfolio: "Este es mi portfolio.",
     know: "Si quieres conocer más sobre mí, sigue bajando.",
     about: "Sobre mí",
     par1:
       "Mi nombre es Alex Gómez Uceda. Vivo en un pueblo de Toledo (España) y soy graduado en Ingeniería del Software por la Universidad Rey Juan Carlos desde el año 2018.",
     par2:
-      "Me considero una persona trabajadora, tranquila y curiosa. La tecnología y el deporte son mis dos grandes pasiones. Pienso que la vida es un continuo aprendizaje y nunca hay que dejar de aprender.",
+      "Me considero una persona trabajadora, tranquila y muy curiosa. La tecnología y el deporte son mis dos grandes pasiones. Pienso que la vida es un continuo aprendizaje y nunca hay que dejar de aprender.",
     par3:
       "Si aún deseas saber más sobre mí, puedes hacerlo en el último apartado.",
     contact: "Contacto",
     cv: "Pulsa en la imagen para descargar mi currículum.",
     email: "Pulsa en la imagen para enviarme un correo electrónico.",
-    github: "Pulsa en la imagen para enviarme un correo electrónico.",};
+    github: "Pulsa en la imagen para ver mis proyectos.",};
 
 var lang=es_ES
 // set all the text
